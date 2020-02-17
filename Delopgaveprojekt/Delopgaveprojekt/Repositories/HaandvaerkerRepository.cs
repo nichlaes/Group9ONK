7828cb34375c86da218485f3e1cbf4291f8fa011
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-02-17 21:15:00 +0100

    HaandvaerkerRepository added and implemented for insert

---
+++ Delopgaveprojekt/Delopgaveprojekt/Repositories/HaandvaerkerRepository.cs
@@ -0,0 +1,47 @@
+﻿using Delopgaveprojekt.DbFactory;
+using Delopgaveprojekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delopgaveprojekt.Repositories
+{
+    public class HaandvaerkerRepository : IHaandvaerkerRepository
+    {
+        private readonly IDbFactory _dbFactory;
+
+        public HaandvaerkerRepository(IDbFactory dbFactory)
+        {
+            _dbFactory = dbFactory;
+        }
+        public void AddHaandvaerker(Haandvaerker haandvaerker)
+        {
+            using (var db = _dbFactory.GetConnection())
+            {
+                db.OpenSharedConnection();
+                db.Insert<Haandvaerker>(haandvaerker);
+            }
+        }
+
+        public void DeleteHaandvaerker(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Haandvaerker GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Haandvaerker> GetHaandvaerkers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHaandvaerker(Haandvaerker haandvaerker)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
