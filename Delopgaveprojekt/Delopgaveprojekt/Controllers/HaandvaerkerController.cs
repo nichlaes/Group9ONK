ee82827dbaad3e0249759a268665ca13818ef9de
Jannik <Jannik.lading@hotmail.dk>
2020-03-02 14:00:53 +0100

    Added controllers

---
+++ Delopgaveprojekt/Delopgaveprojekt/Controllers/HaandvaerkerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Delopgaveprojekt.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Delopgaveprojekt.Controllers
+{
+    [Route("api/[controller]")]
+    public class HaandvaerkerController : ControllerBase
+    {
+
+        private HaandvaerkerRepository haandvaerkerRepository;
+        private readonly ILogger<HaandvaerkerController> _logger;
+
+        public HaandvaerkerController(ILogger<HaandvaerkerController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
