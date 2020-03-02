824dbd024f73c3f6b1b1f12fe3a7084e6845f5e8
Jannik <Jannik.lading@hotmail.dk>
2020-03-02 14:37:43 +0100

    .gitignore is now working

--- Delopgaveprojekt/Delopgaveprojekt/Controllers/VaerktoejController.cs
+++ Delopgaveprojekt/Delopgaveprojekt/Controllers/VaerktoejController.cs
@@ -1,55 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
 
-namespace Delopgaveprojekt.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VaerktoejController : ControllerBase
-    {
+//namespace Delopgaveprojekt.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class VaerktoejController : ControllerBase
+//    {
 
-        private readonly ILogger<VaerktoejController> _logger;
+//        private readonly ILogger<VaerktoejController> _logger;
 
-        public VaerktoejController(ILogger<VaerktoejController> logger)
-        {
-            _logger = logger;
-        }
+//        public VaerktoejController(ILogger<VaerktoejController> logger)
+//        {
+//            _logger = logger;
+//        }
 
-        // GET: api/Vaerktoej
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+//        // GET: api/Vaerktoej
+//        [HttpGet]
+//        public IEnumerable<string> Get()
+//        {
+//            return new string[] { "value1", "value2" };
+//        }
 
-        // GET: api/Vaerktoej/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+//        // GET: api/Vaerktoej/5
+//        [HttpGet("{id}", Name = "Get")]
+//        public string Get(int id)
+//        {
+//            return "value";
+//        }
 
-        // POST: api/Vaerktoej
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+//        // POST: api/Vaerktoej
+//        [HttpPost]
+//        public void Post([FromBody] string value)
+//        {
+//        }
 
-        // PUT: api/Vaerktoej/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+//        // PUT: api/Vaerktoej/5
+//        [HttpPut("{id}")]
+//        public void Put(int id, [FromBody] string value)
+//        {
+//        }
 
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
-    }
-}
+//        // DELETE: api/ApiWithActions/5
+//        [HttpDelete("{id}")]
+//        public void Delete(int id)
+//        {
+//        }
+//    }
+//}
