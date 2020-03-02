49d89ebdcd5638b61d8ac3c8c282d8afc87940d4
Tobias Apollo Lauridsen <apollotoby@gmail.com>
2020-03-02 19:01:48 +0100

    Controllers uses repositories

--- Delopgaveprojekt/Delopgaveprojekt/Controllers/VaerktoejController.cs
+++ Delopgaveprojekt/Delopgaveprojekt/Controllers/VaerktoejController.cs
@@ -1,55 +1,61 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Logging;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Delopgaveprojekt.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
-//namespace Delopgaveprojekt.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class VaerktoejController : ControllerBase
-//    {
+namespace Delopgaveprojekt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaerktoejController : ControllerBase
+    {
 
-//        private readonly ILogger<VaerktoejController> _logger;
+        private readonly ILogger<VaerktoejController> _logger;
+        private IVaerktoejRepository _vaerktoejRepository;
 
-//        public VaerktoejController(ILogger<VaerktoejController> logger)
-//        {
-//            _logger = logger;
-//        }
+        public VaerktoejController(ILogger<VaerktoejController> logger, IVaerktoejRepository vaerktoejRepository)
+        {
+            _vaerktoejRepository = vaerktoejRepository;
+            _logger = logger;
+        }
 
-//        // GET: api/Vaerktoej
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
+        // GET: api/Vaerktoej
+        [HttpGet]
+        public List<Models.Vaerktoej> Get()
+        {
+            return _vaerktoejRepository.GetVaerktoejs();
+        }
 
-//        // GET: api/Vaerktoej/5
-//        [HttpGet("{id}", Name = "Get")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
+        // GET: api/Vaerktoej/5
+        [HttpGet("{id}", Name = "Get")]
+        public Models.Vaerktoej Get(int id)
+        {
+            return _vaerktoejRepository.GetById(id);
+        }
 
-//        // POST: api/Vaerktoej
-//        [HttpPost]
-//        public void Post([FromBody] string value)
-//        {
-//        }
+        // POST: api/Vaerktoej
+        [HttpPost]
+        public void Post([FromBody] Models.Vaerktoej vt)
+        {
+            _vaerktoejRepository.AddVaerktoej(vt);
+        }
 
-//        // PUT: api/Vaerktoej/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody] string value)
-//        {
-//        }
+        // PUT: api/Vaerktoej/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Models.Vaerktoej vt)//Hvad skal der helt præcis  ske her???
+        {
+            _vaerktoejRepository.UpdateVaerktoej(vt);
+        }
 
-//        // DELETE: api/ApiWithActions/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-//    }
-//}
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _vaerktoejRepository.DeleteVaerktoej(id);
+        }
+    }
+}
